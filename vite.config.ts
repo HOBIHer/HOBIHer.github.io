9f1d6a050218c0798775c2529cfe39205789beb1
jliu9100 <liujiaqi110@h-partners.com>
2026-06-09 11:16:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'node',
-    include: ['src/tests/**/*.test.ts'],
-  },
+  base: '/SlaytheFish2/',
 });
